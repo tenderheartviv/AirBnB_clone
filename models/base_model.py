299d76b1d2e68467d4142530a2cefe87ee792c9e
AbuSolaimane <fadilimustafa530@gmail.com>
2023-08-11 10:47:11 -0700

    hjdjd

---
+++ models/base_model.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""
+a module contains base class
+"""
+
+from datetime import datetime
+from uuid import uuid4
+import models #storage
+
+class BaseModel:
+    """the Base class (parent class)"""
+
+    def __init__(self, *args, **kwargs):
+        """the constructor"""
+        DATE_TIME_FORMAT = '%Y-%m-%dT%H:%M:%S.%f'
+        if not kwargs:
+            self.id = str(uuid4())
+            self.created_at = datetime.utcnow()
+            self.updated_at = datetime.utcnow()
+            models.storage.new(self)
+        else:
+            for key, value in kwargs.items():
+                if key in ("updated_at", "created_at"):
+                    self.__dict__[key] = datetime.strptime(
+                        value, DATE_TIME_FORMAT)
+                elif key[0] == "id":
+                    self.__dict__[key] = str(value)
+                else:
+                    self.__dict__[key] = value
+
+    def __str__(self):
+        return "[{}] ({}) {}".format(self.__class__.__name__,
+                                     self.id, self.__dict__)
+
+    def save(self):
+        self.updated_at = datetime.utcnow()
+        models.storage.save()
+
+    def to_dict(self):
+        map_objects = {}
+        for key, value in self.__dict__.items():
+            if key == "created_at" or key == "updated_at":
+                map_objects[key] = value.isoformat()
+            else:
+                map_objects[key] = value
+        map_objects["__class__"] = self.__class__.__name__
+        return map_objects
