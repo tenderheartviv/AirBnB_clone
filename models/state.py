07ae8073290c1ccc54cf0b5957e1bd36e1244eb3
tenderheartviv <vivian.emenike@yahoo.com>
2023-08-12 15:50:35 -0700

    all about console

---
+++ models/state.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+State Module
+"""
+
+from models.base_model import BaseModel
+
+class State(BaseModel):
+    """
+    State class that inherits from BaseModel
+    """
+    name = ""
+
